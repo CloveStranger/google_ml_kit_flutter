34efe24ec90625d8489c6a5d9fa8a7a584e0ab44
ziang zhu <100896401+CloveStranger@users.noreply.github.com>
2026-07-21 12:22:10 +0800

    fix(spm): pin vision kit wrapper fork [TRK-20260721-120133-2585]

--- packages/google_mlkit_object_detection/ios/google_mlkit_object_detection/Package.swift
+++ packages/google_mlkit_object_detection/ios/google_mlkit_object_detection/Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     dependencies: [
         .package(path: "../../../google_mlkit_commons/ios/google_mlkit_commons"),
         .package(
-            url: "https://github.com/arrrrny/google-mlkit-swiftpm",
-            revision: "617e67690b277b7d2063c2e4f367bd0155ee79c0"
+            url: "https://github.com/CloveStranger/google-mlkit-swiftpm",
+            revision: "aac64bbc2601100ab28b2abb0a9a4cd910c55eec"
         )
     ],
     targets: [
