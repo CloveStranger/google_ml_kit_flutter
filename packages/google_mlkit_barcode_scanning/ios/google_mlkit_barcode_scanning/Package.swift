983ef42e8162597c655978d98e59f41a19553bd4
ziang zhu <dcdev_flutter@desktop-zhuziang-mac-mini.local>
2026-07-21 17:30:16 +0800

    fix(spm): align ML Kit commons path for Flutter [TRK-20260721-161413-5692]

--- packages/google_mlkit_barcode_scanning/ios/google_mlkit_barcode_scanning/Package.swift
+++ packages/google_mlkit_barcode_scanning/ios/google_mlkit_barcode_scanning/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["google_mlkit_barcode_scanning"])
     ],
     dependencies: [
-        .package(path: "../../../google_mlkit_commons/ios/google_mlkit_commons"),
+        .package(path: "../google_mlkit_commons"),
         .package(
             url: "https://github.com/CloveStranger/google-mlkit-swiftpm",
             revision: "9b63ccf8d6b4a0bd1dc50aae5a6de0e974983b69"
